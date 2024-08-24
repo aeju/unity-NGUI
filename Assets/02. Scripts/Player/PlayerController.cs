842957cbb036a5d393f314e373a8dbb0cf8a30ff
aeju <aejuchoe@naver.com>
2024-08-24 15:23:03 +0900

    수정: 점프 버튼, 연속 점프 문제 해결
    - 지면에 닿았을 때만, 버튼 활성화
    
    # Conflicts:
    #	Assets/01. Scenes/GameScene.unity

--- Assets/02. Scripts/Player/PlayerController.cs
+++ Assets/02. Scripts/Player/PlayerController.cs
@@ -7,6 +7,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public PlayerStats stats;
+    public UIButton jumpButton;
     
     private Rigidbody2D rb;
     private SpriteRenderer spriteRenderer;
@@ -15,8 +16,8 @@ public class PlayerController : MonoBehaviour
     private float moveHorizontal;
     
     [Header("# 플레이어 상태")]
-    private bool isFacingRight = true;
-    private bool isJumping = false;
+    [SerializeField] private bool isFacingRight = true;
+    [SerializeField] private bool isGrounded = true;
 
     private void Awake()
     {
@@ -25,6 +26,20 @@ public class PlayerController : MonoBehaviour
         animator = GetComponent<Animator>();
         stats = GetComponent<PlayerStats>();
     }
+    
+    private void Start()
+    {
+        // 점프 버튼 초기화 및 이벤트 연결
+        if (jumpButton != null)
+        {
+            UIEventListener.Get(jumpButton.gameObject).onClick += OnJumpButtonClicked;
+            SetJumpButtonState(isGrounded);
+        }
+        else
+        {
+            Debug.LogError("Jump button is not assigned to the PlayerController!");
+        }
+    }
 
     void Update()
     {
@@ -32,9 +47,9 @@ public class PlayerController : MonoBehaviour
         moveHorizontal = Input.GetAxisRaw("Horizontal");
         
         // 점프 입력 감지
-        if (Input.GetKeyDown(KeyCode.LeftAlt) && !isJumping)
+        if (Input.GetKeyDown(KeyCode.LeftAlt))
         {
-            Jump();
+            TryJump();
         }
         
         // 방향 전환 체크
@@ -61,11 +76,22 @@ public class PlayerController : MonoBehaviour
         // Rigidbody2D의 속도 설정
         rb.velocity = movement;
     }
+
+    // 우선 점프 : 시도부터
+    public void TryJump()
+    {
+        if (isGrounded)
+        {
+            Jump();
+        }
+    }
     
+    // 실제 점프 수행 
     public void Jump()
     {
         rb.AddForce(new Vector2(0f, stats.jumpForce), ForceMode2D.Impulse);
-        isJumping = true;
+        isGrounded = false;
+        SetJumpButtonState(false); // 점프 후 버튼 비활성화
     }
     
     // 현재 방향과 이동 방향이 다르면, 캐릭터 보는 방향 뒤집기
@@ -86,12 +112,39 @@ public class PlayerController : MonoBehaviour
         animator.SetBool("IsWalking", isWalking);
     }
     
-    // 바닥에 닿았을 때 점프 가능 상태로 변경
+    // 바닥에 닿았을 때, 점프 가능 상태로 변경
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Ground"))
         {
-            isJumping = false;
+            isGrounded = true;
+            SetJumpButtonState(true); // 점프 버튼 활성화 
         }
+    }
+    
+    /*
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+            SetJumpButtonState(false);
+        }
+    }
+    */
+    
+    // 점프 버튼 활성화 상태 설정 
+    private void SetJumpButtonState(bool enabled)
+    {
+        if (jumpButton != null)
+        {
+            jumpButton.isEnabled = enabled;
+        }
+    }
+
+    // 점프 버튼 클릭 이벤트 핸들러 
+    private void OnJumpButtonClicked(GameObject go)
+    {
+        TryJump();
     }
 }
