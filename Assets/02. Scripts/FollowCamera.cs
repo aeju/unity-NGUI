631f2da060c629923bf957a689985187a23f74cb
aeju <aejuchoe@naver.com>
2024-08-24 19:50:17 +0900

    추가: 기본 카메라 이동

--- Assets/02. Scripts/FollowCamera.cs
+++ Assets/02. Scripts/FollowCamera.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class FollowCamera : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public PlayerController playerController;
+    private Transform target; 
+
+    private Vector3 offset; 
+    private Quaternion initialRotation; 
+    
     void Start()
     {
+        if (playerController != null)
+        {
+            target = playerController.transform;
+        }
+        else
+        {
+            Debug.LogError("PlayerController not found in the scene.");
+        }
         
+        // initial offset : 카메라 - 플레이어 위치
+        offset = transform.position - target.position;
+        
+        initialRotation = transform.rotation;
     }
 
-    // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
+        // 플레이어 위치 + offset
+        Vector3 newPos = target.position + offset;
         
+        // 카메라의 위치 = newPos로 업데이트
+        transform.position = newPos;
+        
+        transform.rotation = initialRotation;
     }
 }
