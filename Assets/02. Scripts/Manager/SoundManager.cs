5be266175a2534a5861a508102e8d81b916ec044
aeju <aejuchoe@naver.com>
2024-08-31 01:40:00 +0900

    추가: BGM 재생

--- Assets/02. Scripts/Manager/SoundManager.cs
+++ Assets/02. Scripts/Manager/SoundManager.cs
@@ -2,17 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SoundManager : MonoBehaviour
+public class SoundManager : Singleton<SoundManager>
 {
-    // Start is called before the first frame update
+    public AudioClip bgmClip;
+    private AudioSource audioSource;
+
+    void Awake()
+    {
+        // AudioSource 컴포넌트 추가 및 설정
+        audioSource = this.gameObject.AddComponent<AudioSource>();
+        audioSource.clip = bgmClip;
+        audioSource.loop = true;
+    }
+    
     void Start()
     {
-        
+        PlayBGM();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PlayBGM()
     {
-        
+        if (audioSource != null && bgmClip != null)
+        {
+            audioSource.Play();
+        }
+    }
+
+    public void StopBGM()
+    {
+        if (audioSource != null)
+        {
+            audioSource.Stop();
+        }
+    }
+
+    public void SetBGMVolume(float volume)
+    {
+        if (audioSource != null)
+        {
+            audioSource.volume = Mathf.Clamp01(volume);
+        }
     }
 }
